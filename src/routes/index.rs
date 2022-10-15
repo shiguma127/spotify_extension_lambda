a94dba5b4d7126fb59ebe6cfedcc6eb0fe912fc3
shiguma127 <shiguma127@gmail.com>
2022-10-16 01:41:13 +0900

    fix: 返すJsonの型の違いを修正

--- src/routes/index.rs
+++ src/routes/index.rs
@@ -1,11 +1,19 @@
-use axum::{response::IntoResponse, http::{self}, Json};
-use rspotify::{AuthCodeSpotify, clients::OAuthClient, model::{AdditionalType}};
+use axum::{
+    http::{self},
+    response::IntoResponse,
+    Json,
+};
+use rspotify::{
+    clients::OAuthClient,
+    model::{AdditionalType, PlayableItem},
+    AuthCodeSpotify,
+};
 
-use crate::{user_client::UserClient, errors::spotify_error::SpotifyError};
+use crate::{errors::spotify_error::SpotifyError, user_client::UserClient};
 
 pub async fn get(client: UserClient) -> impl IntoResponse {
-    let spotify :AuthCodeSpotify = client.into();
-    let result  = spotify
+    let spotify: AuthCodeSpotify = client.into();
+    let result = spotify
         .current_playback(
             None,
             Some(&vec![AdditionalType::Track, AdditionalType::Episode]),
@@ -17,8 +25,20 @@ pub async fn get(client: UserClient) -> impl IntoResponse {
             return SpotifyError::from(error).into_response();
         }
     };
-    match context {
-        Some(cxt)=> Json(cxt).into_response(),
-        None => http::StatusCode::INTERNAL_SERVER_ERROR.into_response()
+
+    // TODO handle no contxt
+    let context = match context {
+        Some(cxt) => cxt,
+        None => return http::StatusCode::INTERNAL_SERVER_ERROR.into_response(),
+    };
+
+    let item = match context.item {
+        Some(item) => item,
+        None => return http::StatusCode::INTERNAL_SERVER_ERROR.into_response(),
+    };
+
+    match item {
+        PlayableItem::Track(track) => Json(&track).into_response(),
+        PlayableItem::Episode(episode) => Json(&episode).into_response(),
     }
 }
